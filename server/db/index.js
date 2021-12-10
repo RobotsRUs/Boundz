5d321196544aafb08862f6f130f1c8c69af93520
Greg Solomon <ghsolomon@gmail.com>
2021-12-09 22:28:28 -0500

    feat: checkout and order functionality working

--- server/db/index.js
+++ server/db/index.js
@@ -5,18 +5,16 @@ const Order = require('./models/Order');
 const User = require('./models/User');
 const Product = require('./models/Product');
 const LineItem = require('./models/LineItem');
-const CartItem = require('./models/CartItem');
 
 // associate models:
 User.belongsToMany(Product, { through: 'Cart' });
 Product.belongsToMany(User, { through: 'Cart' });
 Order.belongsToMany(Product, { through: LineItem });
 Product.belongsToMany(Order, { through: LineItem });
+LineItem.belongsTo(Product);
+LineItem.belongsTo(Order);
 User.hasMany(Order);
 Order.belongsTo(User);
-Product.belongsToMany(User, { through: CartItem });
-User.belongsToMany(Product, { through: CartItem });
-CartItem.belongsTo(Product);
 
 // export db and models:
-module.exports = { db, models: { Order, User, Product, CartItem } };
+module.exports = { db, models: { Order, User, Product, LineItem } };
--- server/db/models/LineItem.js
+++ server/db/models/LineItem.js
@@ -1,12 +1,111 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const Order = require('./Order');
+const Product = require('./Product');
 
 const LineItem = db.define('lineItem', {
   quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
-    defaultValue: 1, //unnecessary?
+    defaultValue: 0,
   },
 });
+
+LineItem.findByUserId = async function (userId) {
+  try {
+    return await this.findAll({
+      include: [
+        Product,
+        {
+          model: Order,
+          where: { userId, fulfilled: false },
+        },
+      ],
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+LineItem.addItem = async function (userId, productId, quantity) {
+  try {
+    const [order] = await Order.findOrCreate({
+      where: { userId, fulfilled: false },
+    });
+    const [cartItem] = await this.findOrCreate({
+      where: {
+        orderId: order.id,
+        productId,
+      },
+      include: Product,
+    });
+    cartItem.quantity += quantity;
+    await cartItem.save();
+    return cartItem;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+LineItem.updateItemQty = async function (userId, productId, quantity) {
+  try {
+    const cartItem = await this.findOne({
+      where: {
+        productId,
+      },
+      include: [
+        Product,
+        {
+          model: Order,
+          where: { userId, fulfilled: false },
+        },
+      ],
+    });
+    if (!cartItem) {
+      return;
+    } else {
+      cartItem.quantity = quantity;
+      await cartItem.save();
+      return cartItem;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+LineItem.removeItem = async function (userId, productId) {
+  try {
+    const cartItem = await this.findOne({
+      where: {
+        productId,
+      },
+      include: [
+        Product,
+        {
+          model: Order,
+          where: { userId, fulfilled: false },
+        },
+      ],
+    });
+    if (!cartItem) {
+      return;
+    } else {
+      await cartItem.destroy();
+      return cartItem;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+LineItem.emptyCart = async function (userId) {
+  try {
+    await Order.destroy({
+      where: { userId, fulfilled: false },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = LineItem;
