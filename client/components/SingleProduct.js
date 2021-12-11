335e03e3cf9664538120e4c81f8192b90f340165
Greg Solomon <ghsolomon@gmail.com>
2021-12-10 20:33:51 -0500

    style: style singleProductView with MaterialUI components

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchProduct, clearProduct, addToCart } from '../store';
 import Loading from './Loading';
 import { formatUSD, qtyArray } from './utils';
 import NativeSelect from '@mui/material/NativeSelect';
-import { FormControl, InputLabel, Grid, Stack } from '@mui/material';
+import {
+  FormControl,
+  InputLabel,
+  Grid,
+  Stack,
+  Typography,
+  Container,
+} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 class SingleProduct extends React.Component {
   constructor(props) {
@@ -14,10 +26,12 @@ class SingleProduct extends React.Component {
     this.state = {
       format: 0,
       qty: 1,
+      tab: 0,
     };
     this.handleFormatChange = this.handleFormatChange.bind(this);
     this.handleQtyChange = this.handleQtyChange.bind(this);
     this.handleAddToCart = this.handleAddToCart.bind(this);
+    this.handleTabChange = this.handleTabChange.bind(this);
   }
   componentDidMount() {
     this.props.fetchProduct(this.props.match.params.productId);
@@ -41,6 +55,9 @@ class SingleProduct extends React.Component {
     const userId = this.props.auth.id;
     this.props.addToCart(userId, this.props.book, +this.state.qty);
   }
+  handleTabChange(evt, newTab) {
+    this.setState({ tab: newTab });
+  }
   render() {
     if (!this.props.book.id) {
       return <Loading />;
@@ -61,77 +78,135 @@ class SingleProduct extends React.Component {
         variations,
       } = this.props.book;
       return (
-        <Grid>
-          <Grid container>
+        <>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
             <Grid item>
               <img className="cover-art" src={imageUrl} />
             </Grid>
             <Grid item>
-              <h2 className="title">{title}</h2>
-              <h3 className="author">by {author}</h3>
-              <p className="price">{formatUSD(price)}</p>
-              <p className="description">{description}</p>
-              <form className="add-to-cart-form">
-                <FormControl>
-                  <InputLabel htmlFor="format">Format</InputLabel>
-                  <NativeSelect
-                    id="format"
-                    name="format"
-                    value={this.state.format}
-                    label="Format"
-                    onChange={this.handleFormatChange}
-                  >
-                    <option value={id}>{format}</option>
-                    {variations.map((variation) => (
-                      <option key={variation.id} value={variation.id}>
-                        {variation.format}
-                      </option>
-                    ))}
-                  </NativeSelect>
-                </FormControl>
-                <FormControl>
-                  <InputLabel htmlFor="qty">Qty</InputLabel>
-                  <NativeSelect
-                    id="qty"
-                    name="qty"
-                    value={this.state.qty}
-                    label="Format"
-                    onChange={this.handleQtyChange}
-                  >
-                    {qtyArray().map((qty) => (
-                      <option key={qty} value={qty}>
-                        {qty}
-                      </option>
-                    ))}
-                  </NativeSelect>
-                </FormControl>
-                <div>
-                  <IconButton
-                    color="primary"
-                    aria-label="add to shopping cart"
-                    onClick={this.handleAddToCart}
-                  >
-                    <AddShoppingCartIcon />
-                  </IconButton>
-                </div>
-              </form>
+              <Grid container direction="column" spacing={4}>
+                <Grid item container direction="column" spacing={1}>
+                  <Grid item>
+                    <Typography variant="h4">{title}</Typography>
+                    <Typography variant="subtitle">by {author}</Typography>
+                    <Typography variant="subtitle2" color="text.secondary">
+                      {formatUSD(price)}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="body1">{description}</Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                      <FormControl>
+                        <InputLabel htmlFor="format">Format</InputLabel>
+                        <NativeSelect
+                          id="format"
+                          name="format"
+                          value={this.state.format}
+                          label="Format"
+                          onChange={this.handleFormatChange}
+                        >
+                          <option value={id}>{format}</option>
+                          {variations.map((variation) => (
+                            <option key={variation.id} value={variation.id}>
+                              {variation.format}
+                            </option>
+                          ))}
+                        </NativeSelect>
+                      </FormControl>
+                    </Grid>
+                    <Grid item>
+                      <FormControl>
+                        <InputLabel htmlFor="qty">Qty</InputLabel>
+                        <NativeSelect
+                          id="qty"
+                          name="qty"
+                          value={this.state.qty}
+                          label="Format"
+                          onChange={this.handleQtyChange}
+                        >
+                          {qtyArray().map((qty) => (
+                            <option key={qty} value={qty}>
+                              {qty}
+                            </option>
+                          ))}
+                        </NativeSelect>
+                      </FormControl>
+                    </Grid>
+                    <Grid item>
+                      <FormControl>
+                        <IconButton
+                          color="primary"
+                          aria-label="add to shopping cart"
+                          onClick={this.handleAddToCart}
+                        >
+                          <AddShoppingCartIcon />
+                        </IconButton>
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
-          <div className="product-body">
-            <p className="summary">{summary}</p>
-            <div className="product-details">
+          <Box sx={{ width: '100%', mt: 4 }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={this.state.tab} onChange={this.handleTabChange}>
+                <Tab label="Description" {...a11yProps(0)} />
+                <Tab label="Details" {...a11yProps(1)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={this.state.tab} index={0}>
+              <Typography>{summary}</Typography>
+            </TabPanel>
+            <TabPanel value={this.state.tab} index={1}>
               <ul>
                 <li>Publisher: {publisher}</li>
                 <li>Length: {length}</li>
                 <li>ISBN13: {ISBN13}</li>
                 <li>Category: {category}</li>
               </ul>
-            </div>
-          </div>
-        </Grid>
+            </TabPanel>
+          </Box>
+        </>
       );
     }
   }
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
 }
 
 const mapState = (state) => ({
